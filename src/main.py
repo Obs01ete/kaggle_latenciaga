1ef05c82203d671d8ac8657983f906533311101a
Dmitrii Khizbullin <dmitrii.khizbullin@gmail.com>
2023-09-25 01:57:48 +0300

    Added test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 
 from pathlib import Path
-from typing import Sequence, Dict
+from typing import Sequence, Dict, Optional
 import numpy as np
 from tqdm import tqdm
+from collections import defaultdict
+from argparse import ArgumentParser
 
 import torch
 from torch.utils.tensorboard.writer import SummaryWriter
@@ -26,30 +28,35 @@ def concat_premade_microbatches(microbatch_list: Sequence[Batch]):
 
 
 class Trainer:
-    def __init__(self):
+    def __init__(self, debug: bool = False):
+        self.debug = debug
+    
         data_root = Path("/home/khizbud/latenciaga/data/npz_all/npz")
 
         self.microbatch_size = 4
 
-        collection = "layout-xla-random"
+        self.collection = "layout-xla-random"
 
         self.train_data = LayoutData(
             data_root,
-            coll=collection,
+            coll=self.collection,
             split="train",
             microbatch_size=self.microbatch_size)
 
         self.val_data = LayoutData(
             data_root,
-            coll=collection,
+            coll=self.collection,
             split="valid")
 
         self.test_data = LayoutData(
             data_root,
-            coll=collection,
+            coll=self.collection,
             split="test")
+        
+        default_worker_threads = 16 # 8
+        self.worker_threads = 0 if self.debug else default_worker_threads
 
-        self.logger = SummaryWriter()
+        self.logger = None
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         print("device=", self.device)
@@ -61,22 +68,25 @@ class Trainer:
         self.batch_size = 10
         # assert self.batch_size == 1, ("Need to check batch.node_opcode "
         #     "is valid before enabling batch > 1")
-        self.val_batch_size = 20
+        self.val_batch_size = 40
         self.test_batch_size = self.val_batch_size
 
         self.loss_op = MeanAbsolutePercentageError().to(self.device)
 
     def train(self):
 
+        if self.logger is None:
+            self.logger = SummaryWriter()
+
         self.iteration = 0
 
-        self.validate()
+        # self.validate()
 
         train_loader = DataLoader(
             self.train_data,
             batch_size=self.batch_size,
             shuffle=True,
-            num_workers=0, #8,
+            num_workers=self.worker_threads,
             pin_memory=True,
             collate_fn=concat_premade_microbatches)
         
@@ -84,7 +94,7 @@ class Trainer:
         
         optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
 
-        max_iterations = 40_000
+        max_iterations = 100_000 # 40_000
         epoch = 0
         
         exit_training = False
@@ -108,8 +118,8 @@ class Trainer:
                     batch.edge_index,
                     batch.batch)
                 
-                print("pred", pred.detach().cpu().numpy())
-                print("target", batch.config_runtime.detach().cpu().numpy())
+                # print("pred", pred.detach().cpu().numpy())
+                # print("target", batch.config_runtime.detach().cpu().numpy())
 
                 loss = self.loss_op(pred, batch.config_runtime)
 
@@ -142,38 +152,52 @@ class Trainer:
             
             epoch += 1
         
-        self.test()
+        self.test("end_of_train_submission.csv")
     
     def validate(self):
-        val_loader = DataLoader(
-            self.val_data,
+        self._validate('valid')
+    
+    def test(self, submission_csv_path: str):
+        self._validate('test', submission_csv_path)
+    
+    def _validate(self, split: str,
+                  submission_csv_path: Optional[str] = None):
+
+        assert split in {'valid', 'test'}
+
+        if submission_csv_path is None:
+            submission_csv_path = "submission_val.csv"
+
+        loader = DataLoader(
+            self.val_data if split == 'valid' else self.test_data,
             batch_size=self.val_batch_size,
             shuffle=False,
-            num_workers=0, #8,
+            num_workers=self.worker_threads,
             pin_memory=True)
         
-        print("len(val_loader)=", len(val_loader))
+        print("len(loader)=", len(loader))
 
-        from collections import defaultdict
         dd = defaultdict(lambda: defaultdict(list))
         loss_list = []
 
-        for i_batch, batch in tqdm(enumerate(val_loader)):
+        for i_batch, batch in tqdm(enumerate(loader)):
 
             batch = batch.to(self.device)
 
             self.model.eval()
 
-            pred = self.model(
-                batch.node_feat,
-                batch.node_opcode,
-                batch.node_config_feat,
-                batch.node_config_ids,
-                batch.edge_index,
-                batch.batch)
+            with torch.no_grad():
+                pred = self.model(
+                    batch.node_feat,
+                    batch.node_opcode,
+                    batch.node_config_feat,
+                    batch.node_config_ids,
+                    batch.edge_index,
+                    batch.batch)
 
-            loss = self.loss_op(pred, batch.config_runtime)
-            loss_list.append(loss.item())
+                if split == 'valid':
+                    loss = self.loss_op(pred, batch.config_runtime)
+                    loss_list.append(loss.item())
 
             # for key in batch.keys:
             #     v = getattr(batch, key)
@@ -186,46 +210,75 @@ class Trainer:
                 dol['pred_list'].append(pr.item())
                 dol['target_list'].append(tg.item())
 
-            # if i_batch >= 5:
-            #     break
+            if self.debug:
+                if i_batch >= 2:
+                    break
         
-        loss_grand = np.mean(np.array(loss_list, dtype=np.float32))
+        if split == 'valid':
+            loss_grand = np.mean(np.array(loss_list, dtype=np.float32))
 
-        kendall_grand_list = []
-        p_value_grand_list = []
+            kendall_grand_list = []
+            p_value_grand_list = []
+            for name, dol in dd.items():
+                pred_all = np.array(dol['pred_list'], dtype=np.float32)
+                target_all = np.array(dol['target_list'], dtype=np.float32)
+
+                kendall, p_value = kendall_rank_corrcoef(
+                    torch.tensor(pred_all), torch.tensor(target_all),
+                    t_test=True, alternative='two-sided')
+
+                kendall_grand_list.append(kendall.item())
+                p_value_grand_list.append(p_value.item())
+
+            kendall_grand = np.mean(kendall_grand_list)
+            p_value_grand = np.mean(p_value_grand_list)
+
+            print(f"{split} kendall=", kendall_grand.item(), "p_value=", p_value_grand.item())
+            print(f"{split} loss = {loss_grand:.5f}")
+            if self.logger is not None:
+                self.logger.add_scalar("val/loss", loss_grand.item(), self.iteration)
+                self.logger.add_scalar("val/kendall", kendall_grand.item(), self.iteration)
+                self.logger.add_scalar("val/p_value", p_value_grand.item(), self.iteration)
+
+        # Prepare submission
         submission_dict: Dict[str, np.ndarray] = dict()
         for name, dol in dd.items():
             pred_all = np.array(dol['pred_list'], dtype=np.float32)
-            target_all = np.array(dol['target_list'], dtype=np.float32)
-
-            kendall, p_value = kendall_rank_corrcoef(
-                torch.tensor(pred_all), torch.tensor(target_all),
-                t_test=True, alternative='two-sided')
-
-            kendall_grand_list.append(kendall.item())
-            p_value_grand_list.append(p_value.item())
-
             # NB: pred_all are guaranteed to be sequential by DataLoader
             submission_ranks = np.argsort(pred_all)
             submission_dict[name] = submission_ranks
 
-        kendall_grand = np.mean(kendall_grand_list)
-        p_value_grand = np.mean(p_value_grand_list)
+        self.save_submission(submission_dict, submission_csv_path)
+        print(f"Saved to {submission_csv_path}")
 
-        print(f"Val loss = {loss_grand:.5f}")
-        print("VAL kendall=", kendall_grand.item(), "p_value=", p_value_grand.item())
-        self.logger.add_scalar("val/loss", loss_grand.item(), self.iteration)
-        self.logger.add_scalar("val/kendall", kendall_grand.item(), self.iteration)
-        self.logger.add_scalar("val/p_value", p_value_grand.item(), self.iteration)
-        pass
-
-    def test(self):
-        pass
+    def save_submission(self,
+                        submission_dict: Dict[str, np.ndarray],
+                        csv_name: str):
+        with open(csv_name, "w") as f:
+            for npz_name, ranks in submission_dict.items():
+                ranks_list = [str(v) for v in list(ranks)]
+                line = self.collection.replace('-', ':') + ":" + npz_name + "," + \
+                    ";".join(ranks_list) + "\n"
+                f.write(line)
+    
+    def load_snapshot(self, snapshot_path: str):
+        self.model.load_state_dict(torch.load(snapshot_path,
+                                              map_location=self.device))
 
 
 def main():
+    parser = ArgumentParser(description='Latenciaga')
+    parser.add_argument('--test-snapshot', action='store', type=str,
+                        help='Provide .pth, get submission.csv')
+
+    args = parser.parse_args()
+
     trainer = Trainer()
-    trainer.train()
+    if args.test_snapshot is not None:
+        trainer.load_snapshot(args.test_snapshot)
+        trainer.test("test_submission.csv")
+    else:
+        trainer.train()
     print("Done")
 
 
